3965715b9795626f9f3f5310e22e54080e9f638b
theEmil <98152798+theEmil@users.noreply.github.com>
2022-01-21 10:25:31 +0100

    Update calcPolyfit.m
    added comments for usability

--- calcPolyfit.m
+++ calcPolyfit.m
@@ -1,15 +1,19 @@
+% x - x-axis values
+% y - y-axis values to fit to
+% n - polynomial degree of fit
+% silent - supress output of functional equation, default is to output the equation
 function Result = calcPolyfit(x, y, n, silent)
     if ~exist('silent','var')
         silent = 0;
     end
     coefficient = polyfit(x,y,n); 
     if(silent == 0)
-        Output = "Geradengleichung:";
+        Output = "functional equation:";
         for i = 1:length(coefficient)
             if coefficient(i) < 0
                 Output = Output +" "+ coefficient(i)+"*x^"+(length(coefficient)-i);
             else
-                Output = Output +" + "+ coefficient(i)+"*x^"+(length(coefficient)-i);
+                Output = Output +" +"+ coefficient(i)+"*x^"+(length(coefficient)-i);
             end
         end
         disp(Output);
